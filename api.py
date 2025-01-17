def34e33b2776a104745cf202468894de4344580
cheroliv <cheroliv@laposte.net>
2025-01-17 20:44:49 +0100

    feat: Initial project setup with CLI, API, and assistant features
    This commit introduces the initial structure of the school project, including:
    
    - Added a `.gitignore` file to exclude unnecessary files.
    - Implemented a basic FastAPI application in `api.py` with a signup endpoint.
    - Created an assistant module in `assistant.py` using Langchain and Ollama.
    - Introduced `assistant_utils.py` for managing environment variables.
    - Added core data classes in `base.py` and `users/signup.py` using Pyrsistent.
    - Created a command-line interface using Typer in `cli.py`.
    - Added data generation and dataset interaction in `data_test.py` and `dataset.py`.
    - Included a sample formation template in `patron-formation.json`.
    - Added a `Pipfile` for dependency management.
    - Added a Github Actions workflow in `.github/workflows/pylint.yml`.
    - Added a webapp sample in `webapp.py` based on Streamlit.
    - Introduced basic user management in `users/user.py`.
    - Added a `README_webapp.adoc` for Streamlit usage.
    - Included `requirements.txt`.
    - Included `school.ipynb` for dev purposes.
    - Introduced `teach.py` for teaching purpose.
    - Included `test_api.py` for API test.
    - Included `test_assistant.py` for testing the assistant.
    - Included `test_cli.py` for testing the CLI.
    - Included `users/test_signup.py` for testing the signup model.
    - Included `test_template_source.py` for testing purpose.
    - Added a users `__init__.py` module.
    - Added a `__init__.py` module.
    - Added a `setup.py` module.
    This commit sets the foundation for further development.

---
+++ api.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+School FastAPI application.
+"""
+
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI
+
+api = FastAPI()
+
+@api.post("/api/users/signup/{signup}")
+async def signup(signup_id: int, q: Union[str, None] = None):
+    return {"signup_id": signup_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run('api:api', reload=True, port=8888)
---
+++ webapp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+
+st.write("Hello world")
+
+# TODO - to run use : python -m streamlit run school.py
+
+# if __name__ == '__main__':
+#     pass
